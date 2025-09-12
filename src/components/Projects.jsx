d6f9883407c994ffbd3447403669b26156184589
Tanishq lather <tanishqlather@gmail.com>
2025-09-12 17:36:11 +0530

    update 1

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -13,7 +13,7 @@ const Projects = () => {
                 <h2 className='font-bold text-xl md:text-3xl pl-8 md:pl-28 pr-2 md:pr-0 pt-10 bg-gradient-to-r from-[#EA8D8D] to-[#A890FE] bg-clip-text text-transparent'>Project 1 :</h2>
                 <h2 className='pl-8 md:pl-28 pr-2 md:pr-0 font-semibold text-2xl text-white mt-4'>Youtube channel - Riyah Sharma</h2>
                 <div className="w-full md:w-3/4 mt-6 pl-8 md:pl-28 pr-2 md:pr-0">
-                    <img src="public/assets/C - Project_1.jpg" alt="Project 1" className='rounded-xl'/>
+                    <img src="./assets/C - Project_1.jpg" alt="Project 1" className='rounded-xl'/>
                 </div>
                 <p className='pl-8 md:pl-28 pr-2 md:pr-0 text-white mt-6 font-medium text-lg md:text-xl'>Creating 150+ Storytekking videos with over total 250k total views.</p>
                 <a href="#" target="_blank" rel="noopener noreferrer" className='pl-8 md:pl-28 pr-2 md:pr-0'>
@@ -26,7 +26,7 @@ const Projects = () => {
                 <h2 className='font-bold text-xl md:text-3xl pl-8 md:pl-28 pr-2 md:pr-0 pt-10 bg-gradient-to-r from-[#EA8D8D] to-[#A890FE] bg-clip-text text-transparent'>Project 2 :</h2>
                 <h2 className='pl-8 md:pl-28 pr-2 md:pr-0 font-semibold text-2xl text-white mt-4'>Brand Collaboration – GlowUp Skincare</h2>
                 <div className="w-full md:w-3/4 mt-6 pl-8 md:pl-28 pr-2 md:pr-0">
-                    <img src="public/assets/C - Project_2.jpg" alt="Project 2" className='rounded-xl'/>
+                    <img src="./assets/C - Project_2.jpg" alt="Project 2" className='rounded-xl'/>
                 </div>
                 <p className='pl-8 md:pl-28 pr-2 md:pr-0 text-white mt-6 font-medium text-lg md:text-xl'>Partnered with GlowUp Skincare to create a mini-campaign that reached 10k+ views.</p>
                 <a href="#" target="_blank" rel="noopener noreferrer" className='pl-8 md:pl-28 pr-2 md:pr-0'>
@@ -39,7 +39,7 @@ const Projects = () => {
                 <h2 className='font-bold text-xl md:text-3xl pl-8 md:pl-28 pr-2 md:pr-0 pt-10 bg-gradient-to-r from-[#EA8D8D] to-[#A890FE] bg-clip-text text-transparent'>Project 3 :</h2>
                 <h2 className='pl-8 md:pl-28 pr-2 md:pr-0 font-semibold text-2xl text-white mt-4'>Instagram Reels Campaign</h2>
                 <div className="w-full md:w-3/4 mt-6 pl-8 md:pl-28 pr-2 md:pr-0">
-                    <img src="public/assets/C - Project_3.jpg" alt="Project 3" className='rounded-xl'/>
+                    <img src="./assets/C - Project_3.jpg" alt="Project 3" className='rounded-xl'/>
                 </div>
                 <p className='pl-8 md:pl-28 pr-2 md:pr-0 text-white mt-6 font-medium text-lg md:text-xl'>Produced a series of 20 reels for a fashion startup, helping them gain 2k followers in a month.</p>
                 <a href="#" target="_blank" rel="noopener noreferrer" className='pl-8 md:pl-28 pr-2 md:pr-0'>
